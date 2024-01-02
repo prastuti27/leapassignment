233644137facb4dc9ed9848828b95ef71cfd904c
Prastuti <praskim143@gmail.com>
2024-01-02 09:54:38 +0545

    bug fixed

--- htmlcss/assignment13/src/routes/auth.ts
+++ htmlcss/assignment13/src/routes/auth.ts
@@ -3,4 +3,5 @@ import { Router } from "express";
 import { signUp } from "../controller/auth";
 const router = Router();
 router.post("/signUp", signUp);
+// router.post("/login",login);
 export default router;
