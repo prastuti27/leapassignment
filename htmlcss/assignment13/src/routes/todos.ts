12271d539338029b225d2d868a7497da16cd0624
Prastuti <praskim143@gmail.com>
2024-01-05 09:54:44 +0545

    backend-pagination

--- htmlcss/assignment13/src/routes/todos.ts
+++ htmlcss/assignment13/src/routes/todos.ts
@@ -2,9 +2,9 @@ import { Router, Request, Response } from "express";
 import { addTodo, getTodos } from "../controller/todos";
 // import { logger } from "../middleware/logger";
 import { validateReqQuery } from "../middleware/validator";
-import {getUserSchema}  from "../schema/users";
+import { getTaskSchema } from "../schema/todo";
 const router = Router();
-router.get("/", validateReqQuery(getUserSchema), getTodos);
+router.get("/", validateReqQuery(getTaskSchema), getTodos);
 router.post("/", addTodo);
 // router.get("/", logger, getTodos);
 
---
+++ htmlcss/assignment13/src/schema/todo.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from "../constant/pagination";
+
+export const getCreateTaskSchema = Joi.object({
+  title: Joi.string().required().messages({
+    "any.required": "Title is required",
+    "string.empty": "Title cannot be empty",
+  }),
+});
+
+export const getTaskSchema = Joi.object({
+  page: Joi.number().integer().min(1).default(DEFAULT_PAGE),
+
+  size: Joi.number().integer().min(1).max(40).default(DEFAULT_PAGE_SIZE),
+
+  search: Joi.string().default(""),
+
+  completed: Joi.boolean().default(false),
+});
